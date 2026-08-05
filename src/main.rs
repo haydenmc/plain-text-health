617196d2eccc0b2850b8687880cea789e4da6d68
Hayden McAfee <hayden@outlook.com>
2026-08-05 15:21:35 -0700

    Add CLI parsing for command and file arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "fitlog", version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Check {
+        #[arg(env = "FITLOG_FILE")]
+        file: PathBuf,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let (name, file) = match &args.command {
+        Command::Check { file } => ("check", file),
+    };
+    println!("command: {}, file: {}", name, file.display());
 }
